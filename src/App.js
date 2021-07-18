72563bf293a55dc964b09b411fb0029fde8f9659
Tatyana Karpova <ta.karpova@list.ru>
2021-07-18 22:04:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import TodoList from './Todo/TodoList';
 
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Купить хлеб'},
+    {id: 2, completed: false, title: 'Купить масло'},
+    {id: 3, completed: false, title: 'Купить молоко'}
+  ]
+
+
   return (<div className='wrapper'>
     <h1>React tutorial</h1>
 
+    <TodoList todos={todos} />
     <TodoList />
   </div>)
 }
